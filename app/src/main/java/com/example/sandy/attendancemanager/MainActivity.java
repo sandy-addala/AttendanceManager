7378e94c2610d3d32240307c8b88f0e6d00981f0
sandy-addala <sandy.addala1996@gmail.com>
2017-06-27 17:01:54 +0530

    Added a new Text view

--- app/src/main/java/com/example/sandy/attendancemanager/MainActivity.java
+++ app/src/main/java/com/example/sandy/attendancemanager/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.sandy.attendancemanager;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button b;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        b = (Button) findViewById(R.id.btnGo);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =  new Intent(MainActivity.this,AddSubjects.class);
+            }
+        });
     }
 }
