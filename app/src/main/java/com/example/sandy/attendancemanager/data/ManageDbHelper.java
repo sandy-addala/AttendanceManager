cd1b84082421c95d7112f439d45844e86cb30a38
sandy-addala <sandy.addala1996@gmail.com>
2017-07-09 15:11:21 +0530

    Added Manage Attendance Activity...ALMOST DONE

---
+++ app/src/main/java/com/example/sandy/attendancemanager/data/ManageDbHelper.java
@@ -0,0 +1,60 @@
+package com.example.sandy.attendancemanager.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.example.sandy.attendancemanager.data.AttendanceContract.SubjectsEntry;
+
+import java.util.ArrayList;
+
+/**
+ * Created by sandy on 09-07-2017.
+ */
+
+public class ManageDbHelper extends SQLiteOpenHelper {
+
+    public final static int DATABASE_VERSION = 1;
+    public final static String DATABASE_NAME = "attendance.db";
+
+    public ManageDbHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, DATABASE_NAME, factory, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {}
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {}
+
+
+    public void presentOn(Object o, int day) {
+        ArrayList<String> subs = (ArrayList<String>) o;
+        SQLiteDatabase db = getWritableDatabase();
+        for(int i=0;i<subs.size();i++){
+            db.execSQL("UPDATE " + SubjectsEntry.TABLE_NAME + " SET "
+                    + SubjectsEntry.COLUMN_PRESENT_CLASSES + " = "
+                    + SubjectsEntry.COLUMN_PRESENT_CLASSES +" +1 "
+                    + " WHERE "+ SubjectsEntry.COLUMN_SUBJECT_NAME + "=\"" + subs.get(i) +"\";" );
+
+            db.execSQL("UPDATE " + SubjectsEntry.TABLE_NAME + " SET "
+                    + SubjectsEntry.COLUMN_TOTAL_CLASSES + " = "
+                    + SubjectsEntry.COLUMN_TOTAL_CLASSES +" +1 "
+                    + " WHERE "+ SubjectsEntry.COLUMN_SUBJECT_NAME + "=\"" + subs.get(i) +"\";" );
+
+        }
+    }
+
+    public void absentOn(Object o, int day) {
+        ArrayList<String> subs = (ArrayList<String>) o;
+        SQLiteDatabase db = getWritableDatabase();
+        for(int i=0;i<subs.size();i++){
+
+            db.execSQL("UPDATE " + SubjectsEntry.TABLE_NAME + " SET "
+                    + SubjectsEntry.COLUMN_TOTAL_CLASSES + " = "
+                    + SubjectsEntry.COLUMN_TOTAL_CLASSES +" +1 "
+                    + " WHERE "+ SubjectsEntry.COLUMN_SUBJECT_NAME + "=\"" + subs.get(i) +"\";" );
+
+        }
+    }
+}
